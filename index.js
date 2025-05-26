ec5324193484e7610dd3c7ed091ef9db72a22cbb
krishnakatipally801 <126202910+krishnakatipally801@users.noreply.github.com>
2025-05-26 15:36:57 -0500

    index.js

--- index.js
+++ index.js
@@ -10,10 +10,12 @@ const connection = snowflake.createConnection({
   account: process.env.SNOWFLAKE_ACCOUNT,
   username: process.env.SNOWFLAKE_USERNAME,
   password: process.env.SNOWFLAKE_PASSWORD,
+  role: process.env.SNOWFLAKE_ROLE,
   warehouse: process.env.SNOWFLAKE_WAREHOUSE,
   database: process.env.SNOWFLAKE_DATABASE,
   schema: process.env.SNOWFLAKE_SCHEMA
 });
+
 
 // Connect once when app starts
 connection.connect((err) => {
